96cb996a4098019923d1cfaae3d145d514bd19a5
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-02-20 19:54:42 -0800

    add user authentication

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
 
 
 //create a withAuth type function that checks sessions and see if the user is admin
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
  res.render('homepage', {
      loggedIn: req.session.loggedIn, 
      is_admin: req.session.is_admin
