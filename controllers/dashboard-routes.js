dd2718f6b59587d586de42d66fe9194111ee13c6
David Manriquez <dmanriqu@my.chemeketa.edu>
2022-02-23 15:09:03 -0800

    unique id

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const withAuth = require('../utils/auth');
-
+// const withAuth = require('../utils/auth');
+const isAdmin = require('../utils/auth');
 
 //create a withAuth type function that checks sessions and see if the user is admin
-router.get('/', withAuth, (req, res) => {
+router.get('/', isAdmin, (req, res) => {
  res.render('dashboard', {
      loggedIn: req.session.loggedIn, 
-    //  is_admin: req.session.is_admin
+    is_admin: req.session.is_admin
     })
 });
 
