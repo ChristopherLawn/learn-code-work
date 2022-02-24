3d32167201ba0379071bac01cba5c515dd5e6075
CLAW <christopher.lawn@yahoo.com>
2022-02-24 14:15:15 -0800

    Add Admin user to Heroku

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,13 @@
 const sequelize = require('../config/connection');
 const seedLanguages = require('./seedLanguages');
+const seedAdminUser = require('./seedAdminUser');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
 
     await seedLanguages();
+
+    await seedAdminUser();
 
     process.exit(0);
 };
